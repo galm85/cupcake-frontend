281f26bc19cde30389d42bf8e0f3e591b7f75fc6
galm85 <galm85@gmail.com>
2022-06-08 13:50:04 +0300

    about page css

--- src/pages/aboutPage.tsx
+++ src/pages/aboutPage.tsx
@@ -1,3 +1,4 @@
+import { Divider, Grid } from '@mui/material';
 import React from 'react'
 
 
@@ -5,8 +6,68 @@ import React from 'react'
 const AboutPage:React.FC = ()=>{
     return (
         <div className="about-page">
-            <h1 className="page-title">About Us</h1>
-            <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi numquam minus neque, quas iste est non itaque nesciunt ad, officiis porro reiciendis modi voluptates consectetur cumque ipsa voluptatum explicabo veritatis.</span><span>Suscipit odio consequuntur non aliquid recusandae, quisquam soluta qui adipisci, dolore nulla officia, vero fugit possimus perferendis facilis delectus ex. Dolorum qui cum, molestiae doloribus totam inventore ad rem assumenda.</span><span>Cum debitis, consequatur quidem harum officia porro possimus dolore necessitatibus, libero ipsum veniam ratione dignissimos tenetur. A necessitatibus fuga dicta in tenetur, accusantium mollitia, ducimus veniam perferendis minus, eos explicabo.</span><span>Perspiciatis magnam tempora ullam non tempore error ad ducimus consequuntur! Deserunt delectus velit ut labore aliquid odio et accusamus reiciendis quo. Natus, eveniet! Velit accusantium quia illo repudiandae, placeat nulla.</span></p>
+            <h1 className="page-title">Our Story</h1>
+            
+            <section className="begin">
+                <h2>How We Began</h2>
+                <p>
+                    In the 1965, Peter found a recipe in her local San Antonio newspaper that inspired her  CupCake. Everyone loved his recipe so much he decided to open a small bakery, but eventually gave it up to focus on raising her children, Jonathan and Roman. He moved her baking equipment to her basement kitchen and continued to sell cakes to some of the best restaurants in town.</p>
+                <p>
+                    In 1975, Peter and his wife Jenna moved to Oragon to try one last time to have their own business. With the last of their savings, they opened The CupCake Factory Bakery and began selling Peter’s cakes to restaurants throughout Oragon
+                </p>
+                
+                <div className="begin-image">
+                    <img src="./images/old1.jpg" alt="cupcake funders" />
+                    <small>
+                        The Founders Family of The Cupcake factory 1969
+                    </small>
+                </div>
+            </section>
+
+            <Divider style={{margin:'40px 0 0 20px'}} />
+
+            <section className="reviews">
+                <h2>The CupCake on the Media</h2>
+                <Grid container>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>"I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours"</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>"I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours"</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>"I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours"</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>"I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours"</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>"I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours"</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                    <Grid item xs={10} md={4}>
+                        <div className="about-page-review">
+                            <h3>I Attempted To Eat 40 Cheesecake Factory Dishes In Two Hours</h3>
+                            <h5>Sara Smith - The Orogon Times</h5>
+                        </div>
+                    </Grid>
+                </Grid>
+            </section>
+
         </div>
     )
 }
